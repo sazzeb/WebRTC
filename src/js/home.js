5b1ea15a8279b2d579699d17a0a271e655853b8b
SAZZEB <sazzeb@mail.sazzeb.com>
2018-05-22 22:21:05 +0100

    First Video Chart Created

---
+++ src/js/home.js
@@ -0,0 +1,18 @@
+import './general';
+import SimpleWebRTC from 'simplewebrtc';
+
+const webrtc = new SimpleWebRTC({
+    localVideoEl: 'localVideo',
+    remoteVideosEl: '',
+    autoRequestMedia: true,
+    debug: false,
+});
+
+class Home {
+    constructor() {
+
+        
+    }
+}
+
+const home = new Home();
